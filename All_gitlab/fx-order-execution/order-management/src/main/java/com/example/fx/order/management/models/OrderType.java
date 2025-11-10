6e297553fefe6365290e1c457fd7a9766eb9e02b
niharikap1 <niharikapatel412@gmail.com>
2025-11-10 15:48:27 +0000

    Unwanted folder removal.

--- All_gitlab/fx-order-execution/order-management/src/main/java/com/example/fx/order/management/models/OrderType.java
+++
@@ -1,5 +0,0 @@
-package com.example.fx.order.management.models;
-
-public enum OrderType {
-    MARKET, LIMIT, STOP_LOSS, LOOP, GAMMA,STOP_LIMIT
-}
--- All_gitlab/fx-order-execution/order-management/src/main/java/com/example/fx/order/management/models/Side.java
+++
@@ -1,5 +0,0 @@
-package com.example.fx.order.management.models;
-
-public enum Side {
-    BUY,SELL
-}
